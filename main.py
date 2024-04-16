a6c96f42936344a9a4c1850e30e524c515066d8f
connortbot <loiconnor8@gmail.com>
2024-04-16 15:05:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ chat_log = [{
     "message": f"You are an AI trained to help create OSL shaders for Blender. Whenever providing shader code, start and end it with '{marking_signature}'"
 }]
 while True:
-    user_message = input()
+    user_message = input("USER: ")
     chat_response = ''
+    print("GLOSSY AI: ", end='')
 
     for event in co.chat_stream(
         chat_history=chat_log,
@@ -17,9 +18,7 @@ while True:
         if event.event_type == "text-generation":
             chat_response += event.text
             print(event.text, end='')
-        # elif event.event_type == "stream-end":
-            # print(f"\nevent.finish_reason\n")
-
+    print("")
     chat_log.append({"role": "USER", "message": user_message})
     chat_log.append({"role": "CHATBOT", "message": chat_response})
     
