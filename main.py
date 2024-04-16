357c048c91606bdb7ac8ef847fc52f8f5a495bde
connortbot <loiconnor8@gmail.com>
2024-04-16 14:41:31 -0400

    Write code within marking signature to osl file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cohere
 co = cohere.Client('<apiKey>')
+marking_signature = "&& glossy-forma &&"
 
 chat_log = [{
     "role": "SYSTEM", 
-    "message": "You are an AI trained to help create OSL shaders for Blender. Whenever providing shader code, start and end it with '==='"
+    "message": f"You are an AI trained to help create OSL shaders for Blender. Whenever providing shader code, start and end it with '{marking_signature}'"
 }]
 while True:
     user_message = input()
@@ -12,15 +13,21 @@ while True:
     for event in co.chat_stream(
         chat_history=chat_log,
         message=user_message
-        
-        # perform web search before answering the question. You can also use your own custom connector.
-        # connectors=[{"id": "web-search"}],
     ):
         if event.event_type == "text-generation":
             chat_response += event.text
             print(event.text, end='')
-        elif event.event_type == "stream-end":
-            print(f"\nevent.finish_reason")
+        # elif event.event_type == "stream-end":
+            # print(f"\nevent.finish_reason\n")
 
     chat_log.append({"role": "USER", "message": user_message})
     chat_log.append({"role": "CHATBOT", "message": chat_response})
+    
+    segmented_answer = chat_response.split(marking_signature)
+    if len(segmented_answer) > 1:
+        code = segmented_answer[1]
+        filename = "shader.osl"
+        
+        # Writing to file
+        with open(filename, 'w') as file:
+            file.write(code)
