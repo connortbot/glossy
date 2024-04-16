e3fdb1b779c862a568db1022e39622e36089a6f7
connortbot <loiconnor8@gmail.com>
2024-04-15 21:11:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import cohere
 co = cohere.Client('<apiKey>')
 
-for event in co.chat_stream(
-    chat_history=[
-    {"role": "USER", "message": "Who discovered gravity?"},
-    {"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
-    ],
-    message="Can you write a short biography about him?",
-    # perform web search before answering the question. You can also use your own custom connector.
-    connectors=[{"id": "web-search"}],
-):
-    if event.event_type == "text-generation":
-        print(event.text, end='')
-    elif event.event_type == "stream-end":
-        print(f"\nevent.finish_reason")
+chat_log = []
+while True:
+    user_message = input()
+    chat_response = ''
+
+    for event in co.chat_stream(
+        chat_history=chat_log,
+        message=user_message
+        
+        # perform web search before answering the question. You can also use your own custom connector.
+        # connectors=[{"id": "web-search"}],
+    ):
+        if event.event_type == "text-generation":
+            chat_response += event.text
+            print(event.text, end='')
+        elif event.event_type == "stream-end":
+            print(f"\nevent.finish_reason")
+
+    chat_log.append({"role": "USER", "message": user_message})
+    chat_log.append({"role": "CHATBOT", "message": chat_response})
